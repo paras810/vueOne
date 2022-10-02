6f68b2da921f078e8d5fcd605f4115c80c820633
paras singh <newparas11422@gmail.com>
2022-10-02 08:10:20 -1200

    components

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const app = Vue.createApp({
 
+})
+app.component('login-form', {
+    template: `
+    <form @submit.prevent.stop="handleSub() ">
+      <h1>{{title}}</h1>
+            
+        <input type="text" :placeholder="form_Name" v-model="form_Name"><br><br>
+        <input type="text" :placeholder="form_Mail" v-model="form_Mail"><br><br>
+        <button>Submit</button>
+      </form>
+    `,
     data() {
         return {
-           greeting : "hello vue 3!" ,
-           anGreet:'good morning',
-           isVisible:false,
+            title: "Login form",
+            form_Name:'Name',
+            form_Mail:'Email'
         }
     },
-    methods:{
-        toggleBox(){
-            this.isVisible = !this.isVisible
-        },
-        greet(miniGreet){
-            console.log(this.anGreet)
-            console.warn(miniGreet)
+    methods: {
+        handleSub(){
+            alert('form submitted')
+            console.warn(this.form_Name, " ", this.form_Mail)
         }
-    }
+    },
 })
 app.mount("#app")
