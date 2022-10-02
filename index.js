a167118e8d5d50b87f48378668d66a528a6d2719
paras singh <newparas11422@gmail.com>
2022-10-02 02:31:53 -1200

    event inn vue

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const app = Vue.createApp({
     data() {
         return {
            greeting : "hello vue 3!" ,
+           anGreet:'good morning',
            isVisible:false,
-           isVisible2:true,
         }
     },
+    methods:{
+        toggleBox(){
+            this.isVisible = !this.isVisible
+        },
+        greet(miniGreet){
+            console.log(this.anGreet)
+            console.warn(miniGreet)
+        }
+    }
 })
 app.mount("#app")
