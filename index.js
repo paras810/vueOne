812c06e0c7403edd07dcece2195affe9f83947d7
paras singh <newparas11422@gmail.com>
2022-10-08 05:16:08 -1200

    life cycle hook

--- index.js
+++ index.js
@@ -1,72 +1,33 @@
-const app = Vue.createApp({});
-app.component("login-form", {
-  template: `
-    <form @submit.prevent.stop="handleSub() ">
-      <h1>{{title}}</h1>
+const app = Vue.createApp({
 
-        <input-form
-        v-for="item,i in inputs"
-        :key="i"
-         v-model="item.value"  
-        :label="item.label"
-        :type="item.type" />
-        <br><br>
-
-        <button>Submit</button>
-      </form>
-    `,
-  components: ["input-form"],
   data() {
-    return {
-      title: "Login form",
-      inputs:[
-        {
-          label:'Email: ',
-          value:'',
-          type:'email'
-        },
-        {
-          label:'Password: ',
-          value:'',
-          type:'password'
-        }
-      ]
-    };
+      return {
+         isVisible:false,
+      }
   },
   methods: {
-    handleSub() {
-      console.log("form submitted");
-      console.warn(this.inputs[0].value, this.inputs[1].value);
+    toggleBox(){
+      this.isVisible = !this.isVisible
     },
-  },
-});
-
-app.component("input-form", {
-  template: `
-  <label >{{label}}
-  <input :type="type" v-model="inputVal">
   
-</label>
-  `,
-props:['label','modelValue','type'],
-computed: {
-  inputVal:{
-    get(){
-      console.log('get function')
-      return this.modelValue;
-    },
-    set(value){
-    //  console.log('set function')
-   
-    this.$emit('update:modelValue',value)
-    }
-  }
-}
-,
-// data(){
-//   return{
-//     inputVal: ''
-//   }
-// }
-});
-app.mount("#app");
+  },
+  updated() {
+    console.warn('main comp. updated')
+  },
+  
+})
+app.component('test-box',{
+  template:`
+  <div class="box" ></div>
+  
+  `,created() {
+    console.log(' test comp. created')
+  },
+  mounted() {
+    console.log('test comp. mounted')
+  },
+  unmounted() {
+    console.warn('test comp. unmounted')
+  },
+})
+app.mount("#app")
